efc0a54a158685874143afbcdaaafc2f47c4d1ed
Christian Agha <christianagha@gmail.com>
2019-11-05 13:26:11 -0500

    Updates

---
+++ precache-manifest.f28977293d8bf28e2d5bfff73c69a1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d00cfd3aee87ccd58cf2c1ff0a7a21",
+    "url": "/react-practice-robofriends/index.html"
+  },
+  {
+    "revision": "9ab9a59def5155730afd",
+    "url": "/react-practice-robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ea5fb369b5ae833fe772",
+    "url": "/react-practice-robofriends/static/css/main.e045814a.chunk.css"
+  },
+  {
+    "revision": "9ab9a59def5155730afd",
+    "url": "/react-practice-robofriends/static/js/2.bc2b8b11.chunk.js"
+  },
+  {
+    "revision": "ea5fb369b5ae833fe772",
+    "url": "/react-practice-robofriends/static/js/main.66f4f4f6.chunk.js"
+  },
+  {
+    "revision": "5d8c5824ca1d3d2cf2e4",
+    "url": "/react-practice-robofriends/static/js/runtime~main.cc0a9d9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-practice-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
